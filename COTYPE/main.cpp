615e5e9350f190a1d50232b132bcebdc6f108236
yohanalydia <yohanaalydia@gmail.com>
2024-12-07 23:29:11 +0700

    Add files via upload

--- COTYPE/main.cpp
+++ COTYPE/main.cpp
@@ -1,6 +1,47 @@
-#include "COTYPE.h"
+#include "tubes.h"
+#include <cstdlib>
+
+
+using namespace std;
 
 int main(){
+    adrKata kataPertama;
+    adrBaris B;
+    adrHuruf cursor = nullptr;
 
-    return 0;
+    Line = createBaris();
+    B = createElemBaris();
+    Line.first = B;
+    Line.last = B;
+    kataPertama = createElemKata();
+    B -> firstKata = kataPertama;
+    B -> lastKata = kataPertama;
+
+    cursor = createElemHuruf('|');
+    kataPertama->firstHuruf = cursor;
+    kataPertama->lastHuruf = cursor;
+    activeBaris = Line.first;
+    activeKata = kataPertama;
+
+    char ch;
+    while (1) {
+        if (_kbhit()) {  // memeriksa apakah tombol sudah ditekan
+            ch = _getch();  // membaca karakter tanpa menunggu Enter
+
+            if (ch == 27) {  // kondisi jika ESC ditekan
+                break;  // keluar dari loop jika ESC ditekan
+            } else {
+                inputHandler(cursor, ch);  // menangani input selain ESC
+            }
+
+            // clear layar dan menampilkan teks yang diperbarui
+            system("cls");
+            cout << "Teks: " << endl;
+            displayKata();
+            cout << "--------" <<endl;
+            cout << "activeKata firstHuruf: " << activeKata -> firstHuruf -> info << endl;
+            cout << "activeKata lastHuruf: " << activeKata -> lastHuruf -> info << endl;
+        }
+    }
+    keteranganText();
 }
